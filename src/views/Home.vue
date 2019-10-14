3e62e8585660cfa275d6fbc7c53064d29d981dc4
Masato Kiryu <kiri9120@gmail.com>
2019-10-14 16:41:41 +0900

    update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,19 @@
 <template>
-  <HelloWorld />
+    <v-row no-gutters>
+      <v-col v-for="(item, index) in topItems" :key="index" cols="12" sm="4">
+        <v-hover v-slot:default="{ hover }">
+          <v-card outlined tile :style='{ backgroundImage: `url(${item.image})` }' :to="item.link">
+            <v-responsive :aspect-ratio="16/9">
+            <v-expand-transition>
+              <div v-if="hover" class="d-flex transition-fast-in-fast-out brown darken-2 v-card--reveal display-2 white--text" style="height: 100%;">
+              {{item.name}}
+              </div>
+            </v-expand-transition>
+            </v-responsive>
+          </v-card>
+        </v-hover>
+      </v-col>
+    </v-row>
 </template>
 
 <script>
@@ -9,5 +23,68 @@ export default {
   components: {
     HelloWorld,
   },
+  name: 'App',
+  data () {
+    return {
+        topItems: [
+          {
+            name: 'TOP',
+            image: require('../assets/flower01.jpg'),
+            link: '/'
+          },
+          {
+            name: 'ABOUT',
+            image: require('../assets/flower02.jpg'),
+            link: '/about'
+          },
+          {
+            name: 'MEMBER',
+            image: require('../assets/flower03.jpg'),
+            link: '/member'
+          },
+          {
+            name: 'CONTACT',
+            image: require('../assets/flower04.jpg'),
+            link: '/contact'
+          },
+          {
+            name: 'NEWS',
+            image: require('../assets/flower05.jpg'),
+            link: '/news'
+          },
+          {
+            name: 'GALLERY',
+            image: require('../assets/flower06.jpg'),
+            link: '/gallery'
+          },
+          {
+            name: 'タイトル7',
+            image: require('../assets/flower07.jpg'),
+            link: '/'
+          },
+          {
+            name: 'タイトル8',
+            image: require('../assets/flower08.jpg'),
+            link: '/'
+          },
+          {
+            name: 'タイトル9',
+            image: require('../assets/flower09.jpg'),
+            link: '/'
+          },
+        ]
+    }
+  }
+
 };
 </script>
+<style scoped lang="scss">
+  .v-card--reveal {
+    align-items: center;
+    bottom: 0;
+    justify-content: center;
+    opacity: .5;
+    position: absolute;
+    width: 100%;
+  }
+</style>
