70471b3387269d391e5132a28c58f3ed6b734529
Masato Kiryu <kiri9120@gmail.com>
2019-10-15 14:11:56 +0900

    add parralax

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="member">
     <v-container>
-      <h1>これはギャラリーページです</h1>
+      <h1 class="display-4 my-12">Gallery</h1>
+      <v-parallax src="../assets/flower-bg.jpg" height="400">
+      </v-parallax>
     </v-container>
   </div>
 </template>
+
+<style scoped lang="scss">
+</style>
