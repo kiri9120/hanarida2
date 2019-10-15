45a2b0469e76ec9406fb1095fb92deebccfe713f
Masato Kiryu <kiri9120@gmail.com>
2019-10-15 23:03:53 +0900

    change public path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   outputDir: 'docs',
   assetsDir: './',
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/hanarida2/'
+    : '/',
   "transpileDependencies": [
     "vuetify"
   ]
