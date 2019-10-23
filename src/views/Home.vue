8d059895e95e461d16c9bdf74e02fcc12e5842d1
Masato Kiryu <kiri9120@gmail.com>
2019-10-23 18:25:53 +0900

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,22 @@
 <template>
-    <v-row no-gutters>
-      <v-col v-for="(item, index) in topItems" :key="index" cols="4" style="height: calc(calc(100vh - 56px) / 3)">
+    <v-row no-gutters v-if="$vuetify.breakpoint.xsOnly">
+      <v-col v-for="(item, index) in topItems" :key="`first-${index}`" cols="6" style="height: calc(calc(100vh - 56px) / 3)">
+        <v-hover v-slot:default="{ hover }">
+          <v-card outlined tile :style='{ backgroundImage: `url(${item.image})` }' :to="item.link" style="height: 100%; position: relative">
+            <p class="top-text">
+              {{item.name}}
+            </p>
+          </v-card>
+        </v-hover>
+      </v-col>
+    </v-row>
+    <v-row no-gutters v-else>
+      <v-col v-for="(item, index) in topItems" :key="`second-${index}`" cols="4" style="height: calc(calc(100vh - 56px) / 2)">
         <v-hover v-slot:default="{ hover }">
           <v-card outlined tile :style='{ backgroundImage: `url(${item.image})` }' :to="item.link" style="height: 100%;">
             <v-expand-transition>
               <div v-if="hover" class="d-flex transition-fast-in-fast-out brown darken-2 v-card--reveal display-2 white--text">
-              {{item.name}}
+                {{item.name}}
               </div>
             </v-expand-transition>
           </v-card>
@@ -15,19 +26,13 @@
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
-
 export default {
-  components: {
-    HelloWorld,
-  },
-  name: 'App',
   data () {
     return {
       topItems: [
         {
           name: 'TOP',
-          image: require('../assets/flower01.jpg'),
+          image: require('../assets/flower02.jpg'),
           link: '/'
         },
         {
@@ -37,38 +42,23 @@ export default {
         },
         {
           name: 'MEMBER',
-          image: require('../assets/flower03.jpg'),
+          image: require('../assets/flower02.jpg'),
           link: '/member'
         },
         {
           name: 'CONTACT',
-          image: require('../assets/flower04.jpg'),
+          image: require('../assets/flower02.jpg'),
           link: '/contact'
         },
         {
           name: 'NEWS',
-          image: require('../assets/flower05.jpg'),
+          image: require('../assets/flower02.jpg'),
           link: '/news'
         },
         {
           name: 'GALLERY',
-          image: require('../assets/flower06.jpg'),
+          image: require('../assets/flower02.jpg'),
           link: '/gallery'
-        },
-        {
-          name: 'ページ7',
-          image: require('../assets/flower07.jpg'),
-          link: '/'
-        },
-        {
-          name: 'ページ8',
-          image: require('../assets/flower08.jpg'),
-          link: '/'
-        },
-        {
-          name: 'ページ9',
-          image: require('../assets/flower09.jpg'),
-          link: '/'
         }
       ]
     }
@@ -84,5 +74,14 @@ export default {
     position: absolute;
     width: 100%;
     height: 100%;
+  }
+
+  .top-text {
+    margin-bottom: 0;
+    color: white;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
   }
 </style>
