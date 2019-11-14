718783f62791245f9bad54a187bf4e12d505abec
Masato Kiryu <kiri9120@gmail.com>
2019-11-14 23:27:28 +0900

    change home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-row no-gutters v-if="$vuetify.breakpoint.xsOnly">
+  <!-- <v-row no-gutters v-if="$vuetify.breakpoint.xsOnly">
       <v-col v-for="(item, index) in topItems" :key="`first-${index}`" cols="6" style="height: calc(calc(100vh - 56px) / 3)">
         <v-hover v-slot:default="{ hover }">
           <v-card outlined tile :style='{ backgroundImage: `url(${item.image})` }' :to="item.link" style="height: 100%; position: relative; border: none;">
@@ -22,7 +22,24 @@
           </v-card>
         </v-hover>
       </v-col>
-    </v-row>
+  </v-row>-->
+  <div>
+    <div class="bg-image-loop"></div>
+    <v-container>
+      <div class="story py-12">
+        <v-row>
+          <v-col cols="6">
+            <h2 class="display-4 mb-12">Story</h2>
+            <p class="mb-12">それは、２人と１匹の<br>レシピストのある暮らし</p>
+            <v-img src="https://picsum.photos/510/300?random" aspect-ratio="1" max-width="500" class="ml-auto"></v-img>
+          </v-col>
+          <v-col cols="6">
+            <v-img src="https://picsum.photos/510/300?random" aspect-ratio="0.8" max-width="500"></v-img>
+          </v-col>
+        </v-row>
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -84,4 +101,22 @@ export default {
     left: 50%;
     transform: translateX(-50%);
   }
+
+.bg-image-loop {
+	width: 100%;
+	height: 600px; /* 画像の高さを指定 */
+	position: relative;
+	background: url("../assets/slide-sample.jpg") repeat-x 0 0;
+	background-size: auto 100%;
+	animation: bgSlider 200s linear infinite;
+	padding: 0;
+
+  &:hover {
+    animation-play-state: paused;
+  }
+}
+@keyframes bgSlider {
+	from { background-position: 0 0; }
+    to { background-position: -13172px 0; }
+}
 </style>
